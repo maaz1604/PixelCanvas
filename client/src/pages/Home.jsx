4cc4e52d57277d97e76278515d55416e79419ea1
Maaz Amir <amirmaaz16@gmail.com>
2024-12-22 23:45:49 +0530

    1/4 code added in this project

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Steps/>
+    </>
+  )
+}
+
+export default Home
